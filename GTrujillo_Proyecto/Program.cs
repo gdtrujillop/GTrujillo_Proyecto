cf68a8578918ec04d0d316d000ffc518682b43f9
Giuliano Trujillo <didier1402@gmail.com>
2025-04-11 19:16:34 -0500

    Agregar archivos de proyecto.

---
+++ GTrujillo_Proyecto/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GTrujillo_Proyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
